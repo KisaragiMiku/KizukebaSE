3192a991bff61183dfc6f11805e88e970c4bc35e
技術ME開発部　河野 英明 <h-kawano@tnw.top-tokyo.co.jp>
2016-09-08 16:48:57 +0900

    バージョン管理ルール記載

---
+++ KizukebaSE/Controllers/RuleController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KizukebaSE.Controllers
+{
+    public class RuleController : Controller
+    {
+        //
+        // GET: /Rule/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
